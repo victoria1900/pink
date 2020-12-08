3de88ecde7e11049467e752670535caab1d27020
Victoria1900 <victoriadmitrievna@bk.ru>
2020-12-08 14:35:30 +0500

    help

---
+++ source/js/swiper-first.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper('.pink-main-section__swiper-container', {
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
---
+++ source/js/swiper-second.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
